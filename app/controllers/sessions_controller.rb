be2a062033752d970e0ce540c882a11439a383e6
Super-WeiDong <weidongwang@brandeis.edu>
2022-10-16 23:09:22 -0400

    authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,24 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
   def create
-    render 'new', status: :unprocessable_entity
+    user = User.find_by(email: params[:session][:email].downcase)
+    # authenticate method provided by has_secure_password
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+      reset_session
+      log_in user
+      redirect_to user
+    else
+      # Create an error message.
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def destroy
+    log_out
+    redirect_to root_url, status: :see_other
   end
 end
